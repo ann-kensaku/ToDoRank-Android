fdfdba3ed16bb78f51c1c2bb498d6d30e5c2b1a3
t-fuji109 <fujita-tomoko-sz@ynu.jp>
2019-08-17 22:11:48 +0900

    onClickの追加

--- app/src/main/java/jp/ann/kensaku/todorank/MainActivity.kt
+++ app/src/main/java/jp/ann/kensaku/todorank/MainActivity.kt
@@ -2,9 +2,13 @@ package jp.ann.kensaku.todorank
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.ViewManager
+import android.widget.AdapterView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.concurrent.ScheduledExecutorService
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,11 +27,24 @@ class MainActivity : AppCompatActivity() {
         itemList.add(Item("やること4"))
 
         viewAdapter = RecyclerAdapter(itemList)
+        //binding.recyclerView.adapter = viewAdapter
 
         recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply{
             setHasFixedSize(true)
 
             adapter = viewAdapter
         }
+
+        viewAdapter.setOnItemClickListener(object: RecyclerAdapter.OnItemClickListener {
+            override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onClick(view: View, data: Item) {
+                Toast.makeText(applicationContext, data.title, Toast.LENGTH_SHORT).show()
+
+            }
+
+        })
     }
 }
