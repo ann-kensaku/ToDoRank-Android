70c9919701ff7b24f303f7d85976b02cc34e0780
t-fuji109 <fujita-tomoko-sz@ynu.jp>
2019-08-27 23:22:50 +0900

    編集ダイアローグの追加

--- app/src/main/java/jp/ann/kensaku/todorank/MainActivity.kt
+++ app/src/main/java/jp/ann/kensaku/todorank/MainActivity.kt
@@ -1,10 +1,13 @@
 package jp.ann.kensaku.todorank
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 
 
@@ -13,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerAdapter
 
+    @SuppressLint("InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,9 +29,18 @@ class MainActivity : AppCompatActivity() {
         itemList.add(Item("やること4"))
 
         viewAdapter = RecyclerAdapter(itemList) {
-            val intent = Intent(applicationContext, ItemEditActivity::class.java)
+            /*val intent = Intent(applicationContext, ItemEditActivity::class.java)
             intent.putExtra("title", it.title)
-            startActivity(intent)
+            startActivity(intent)*/
+            val alertDialogBuilder = AlertDialog.Builder(this)
+            val inflater = layoutInflater
+            alertDialogBuilder.setTitle("todoListの編集")
+            val dialogLayout = inflater.inflate(R.layout.edit_dialog, null)
+            val editText = dialogLayout.findViewById<EditText>(R.id.edit_text)
+            editText.setText(it.title)
+            alertDialogBuilder.setView(dialogLayout)
+            //alertDialogBuilder.setPositiveButton("OK")
+            alertDialogBuilder.show()
         }
 
         recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply{
