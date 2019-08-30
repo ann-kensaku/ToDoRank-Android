bca000fa7357cee6a08d34c409a924d07f38701c
t-fuji109 <fujita-tomoko-sz@ynu.jp>
2019-08-30 11:32:53 +0900

    suppressLint消去

--- app/src/main/java/jp/ann/kensaku/todorank/MainActivity.kt
+++ app/src/main/java/jp/ann/kensaku/todorank/MainActivity.kt
@@ -19,7 +19,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerAdapter
 
-    @SuppressLint("InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
