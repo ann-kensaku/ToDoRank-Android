8a742d6059f8350254f688abd323e4cf0a632fa7
t-fuji109 <fujita-tomoko-sz@ynu.jp>
2019-09-17 21:01:29 +0900

    viewModelの追加

---
+++ app/src/main/java/jp/ann/kensaku/todorank/TodoViewModel.kt
@@ -0,0 +1,24 @@
+package jp.ann.kensaku.todorank
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class TodoViewModel(application: Application): AndroidViewModel(application) {
+    private val repository: TodoRepository
+
+    val allTodos: LiveData<List<Item>>
+
+    init {
+        val todoDao = TodoDatabase.getDatabase(application, viewModelScope).todoDao()
+        repository = TodoRepository(todoDao)
+        allTodos = repository.allTodos
+    }
+
+    fun insert(todo: Item) = viewModelScope.launch {
+        repository.insert(todo)
+    }
+
+}
