51198d880da2af4324474f30c9da481e530b341d
t-fuji109 <fujita-tomoko-sz@ynu.jp>
2019-09-17 20:56:28 +0900

    roomのための変更 itemをprimary keyに変更

--- app/src/main/java/jp/ann/kensaku/todorank/Item.kt
+++ app/src/main/java/jp/ann/kensaku/todorank/Item.kt
@@ -1,4 +1,14 @@
 package jp.ann.kensaku.todorank
 
-data class Item(val title: String) {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todo_table")
+data class Item (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    @ColumnInfo(name = "title")
+    val title: String? = null
+)
