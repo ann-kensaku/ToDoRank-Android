7f487b39f89ec0b7b87441536a41b2552fe241c4
t-fuji109 <fujita-tomoko-sz@ynu.jp>
2019-10-01 20:34:55 +0900

    import文の整理

--- app/src/main/java/jp/ann/kensaku/todorank/TodoDatabase.kt
+++ app/src/main/java/jp/ann/kensaku/todorank/TodoDatabase.kt
@@ -5,8 +5,6 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 @Database(entities = [Item::class], version = 1)
 abstract class TodoDatabase: RoomDatabase() {
