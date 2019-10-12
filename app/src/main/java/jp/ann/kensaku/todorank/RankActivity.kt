4de020db393842898a377199c4e61be271d22732
t-fuji109 <fujita-tomoko-sz@ynu.jp>
2019-10-12 10:07:29 +0900

    rankactivityの作成

---
+++ app/src/main/java/jp/ann/kensaku/todorank/RankActivity.kt
@@ -0,0 +1,57 @@
+package jp.ann.kensaku.todorank
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.MotionEvent
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class RankActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rank)
+
+        val intent = getIntent()
+
+        val dragTextView: TextView = findViewById(R.id.drag_text)
+        val compareText: TextView = findViewById(R.id.compare_text)
+
+        val target_text = intent.getStringExtra("NEWITEM")
+        val compare_text = intent.getStringExtra("COMPAREITEM")
+        dragTextView.setText(target_text)
+        compareText.setText(compare_text)
+
+        //画面のサイズを取得する
+        val dm = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(dm)
+        val width = dm.widthPixels
+
+
+        View.OnTouchListener(function = { view, motionEvent ->
+            if (motionEvent.action == MotionEvent.ACTION_MOVE) {
+                view.y = motionEvent.getRawY()
+                view.x = motionEvent.getRawX()
+            }
+
+            if (motionEvent.action == MotionEvent.ACTION_UP) {
+                if (motionEvent.getRawX() >= width * 2 / 3 ) {
+                    val rankIntent = Intent()
+                    rankIntent.putExtra("ANSWER", 1)
+                    setResult(RESULT_OK, rankIntent)
+                    finish()
+                } else if(motionEvent.getRawX() <= width / 3){
+                    val rankIntent = Intent()
+                    rankIntent.putExtra("ANSWER", -1)
+                    setResult(RESULT_OK, rankIntent)
+                    finish()
+                }
+
+            }
+            true
+        })
+    }
+}
+
