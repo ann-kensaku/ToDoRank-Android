d823e48782777dfe9124f130be72b4ac5c599ce7
shiita0903 <shiita0903@gmail.com>
2019-10-14 17:44:45 +0900

    メソッドを切り出してリファクタ

--- app/src/main/java/jp/ann/kensaku/todorank/RankActivity.kt
+++ app/src/main/java/jp/ann/kensaku/todorank/RankActivity.kt
@@ -24,33 +24,36 @@ class RankActivity : AppCompatActivity() {
             binding.compareString = getStringExtra("COMPAREITEM")
         }
 
-        //画面のサイズを取得する
-        val dm = DisplayMetrics()
-        windowManager.defaultDisplay.getMetrics(dm)
-        val width = dm.widthPixels
+        // 画面のサイズを取得する
+        val width = DisplayMetrics().let { dm ->
+            windowManager.defaultDisplay.getMetrics(dm)
+            dm.widthPixels
+        }
 
         binding.targetText.setOnTouchListener { view, motionEvent ->
-            if (motionEvent.action == MotionEvent.ACTION_MOVE) {
-                view.y = motionEvent.getRawY() - view.height / 2
-                view.x = motionEvent.getRawX() - view.width / 2
-            }
-
-            if (motionEvent.action == MotionEvent.ACTION_UP) {
-                if (motionEvent.getRawX() >= width * 2 / 3) {
-                    val rankIntent = Intent()
-                    rankIntent.putExtra("ANSWER", 1)
-                    setResult(RESULT_OK, rankIntent)
-                    finish()
-                } else if (motionEvent.getRawX() <= width / 3) {
-                    val rankIntent = Intent()
-                    rankIntent.putExtra("ANSWER", -1)
-                    setResult(RESULT_OK, rankIntent)
-                    finish()
+            when (motionEvent.action) {
+                MotionEvent.ACTION_MOVE -> {
+                    // TODO: 位置調整する。多分ActionBar分ずれている
+                    view.y = motionEvent.rawY - view.height / 2
+                    view.x = motionEvent.rawX - view.width / 2
                 }
-
+                MotionEvent.ACTION_UP -> {
+                    if (motionEvent.rawX >= width * 2 / 3) {
+                        setAnswer(1)
+                        finish()
+                    } else if (motionEvent.rawX <= width / 3) {
+                        setAnswer(-1)
+                        finish()
+                    }
+                }
             }
             true
         }
     }
-}
 
+    private fun setAnswer(value: Int) {
+        setResult(RESULT_OK, Intent().apply {
+            putExtra("ANSWER", value)
+        })
+    }
+}
